40a910e662a1e8e8cb00b9b6407ed4204da9f12f
Mirbek Askerov <mirbek.askerov@gmail.com>
2017-05-16 01:09:47 +0600

    Refactoring user model spec, compare user.id with object.user_id

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,28 +8,30 @@ RSpec.describe User do
   it { should validate_presence_of :password }
 
   let(:current_user) { create(:user) }
-  let(:another_user) { create(:user) }
   let(:question) { create(:question, user: current_user) }
   let(:answer) { create(:answer, user: current_user, question: question) }
+  let(:another_user) { create(:user) }
+  let(:falsy_question) { create(:question) }
+  let(:falsy_answer) { create(:answer) }
 
   describe 'author_of' do
     context 'compares the id of two objects, return true' do
       it 'should equal user id to question id' do
-        expect(current_user.author_of?(question)).to eq true
+        expect(current_user).to be_author_of(question)
       end
 
       it 'should equal user id to answer id' do
-        expect(current_user.author_of?(answer)).to eq true
+        expect(current_user).to be_author_of(answer)
       end
     end
 
     context 'compares the id of two objects, return false' do
       it 'should not equal user id to question id' do
-        expect(another_user.author_of?(question)).to eq false
+        expect(another_user).to_not be_author_of(falsy_question)
       end
 
       it 'should not equal user id to answer id' do
-        expect(another_user.author_of?(answer)).to eq false
+        expect(another_user).to_not be_author_of(falsy_answer)
       end
     end
   end
