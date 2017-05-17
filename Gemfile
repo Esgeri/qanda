7c5c0ce3ae56833706c9dbce1440d704b9facf39
Mirbek Askerov <mirbek.askerov@gmail.com>
2017-05-17 10:14:21 +0600

    Add gems 'selenium-webdriver & chromedriver-helper version 1.0.0' for browser Chrome

--- Gemfile
+++ Gemfile
@@ -40,7 +40,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
   # Adds support for Capybara system testing and selenium driver
-  gem 'selenium-webdriver'
+  # gem 'selenium-webdriver'
 end
 
 group :development do
@@ -60,6 +60,8 @@ gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 group :test, :development do
   gem 'rspec-rails'
   gem 'factory_girl_rails'
+  gem 'selenium-webdriver'
+  gem "chromedriver-helper", "1.0.0"
 end
 
 group :test do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'shoulda/matchers'
 require 'spec_helper'
 require 'rspec/rails'
+require 'capybara'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -26,6 +27,10 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
+
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
