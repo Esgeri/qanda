fd852f34f0f01bdadd694a2b644b05ebdacfdf78
Mirbek Askerov <mirbek.askerov@gmail.com>
2017-06-01 14:19:14 +0600

    Add files to answer

--- spec/acceptance/answers/add_files_to_answer_spec.rb
+++ spec/acceptance/answers/add_files_to_answer_spec.rb
@@ -14,7 +14,7 @@ feature 'Add files to answer', %q{
       visit question_path(question)
     end
 
-    scenario 'User adds file when asks question' do
+    scenario 'User adds file when asks question', js: true do
       fill_in 'Your Answer', with: 'My answer'
       attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
       click_on 'Post Your Answer'
