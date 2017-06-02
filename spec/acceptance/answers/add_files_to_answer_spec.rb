d85a599846160007e0804e6b4bcefba573eb7022
Mirbek Askerov <mirbek.askerov@gmail.com>
2017-06-02 14:23:15 +0600

    Add multiple file uploading for question and answer

--- spec/acceptance/answers/add_files_to_answer_spec.rb
+++ spec/acceptance/answers/add_files_to_answer_spec.rb
@@ -23,4 +23,18 @@ feature 'Add files to answer', %q{
         expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
       end
     end
+
+    scenario 'User adds several files', js: true do
+      fill_in 'Your Answer', with: 'My answer'
+      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+
+      click_on 'Show attachment form'
+      within page.all('.nested-fields').last do
+        attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+      end
+      click_on 'Post Your Answer'
+
+      expect(page).to have_content 'Attachments'
+      expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
+    end
 end
--- spec/acceptance/questions/add_files_to_question_spec.rb
+++ spec/acceptance/questions/add_files_to_question_spec.rb
@@ -21,4 +21,19 @@ feature 'Add files to question', %q{
 
       expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
     end
+
+    scenario 'User can adds several files' do
+      fill_in 'Title', with: 'Test question'
+      fill_in 'Body', with: 'Text text text'
+      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+
+      click_on 'Show attachment form'
+      within page.all('.nested-fields').last do
+        attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+      end
+      click_on 'Create'
+
+      expect(page).to have_content 'Attachments'
+      expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
+    end
 end
