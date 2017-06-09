7a8fdfbaeeabbaa70efafe9ee539b80aea10adf3
Mirbek Askerov <mirbek.askerov@gmail.com>
2017-06-09 14:21:05 +0600

    Added model shared_examples for attachable & has_user

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
+  it_behaves_like 'has_user'
+  it_behaves_like 'attachable'
+
   it { should belong_to :question }
-  it { should belong_to :user }
-  it { should have_many :attachments }
 
   it { should validate_presence_of :body }
-
-  it { should accept_nested_attributes_for :attachments }
 
   describe 'best answer' do
     let!(:user) { create(:user) }
