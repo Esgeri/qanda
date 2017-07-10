b618ae67aecfc5bd62843f25acbcdf2a51a7be44
Mirbek Askerov <mirbek.askerov@gmail.com>
2017-07-11 01:54:32 +0600

    Refactoring: AttachmentsController with responders

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,8 +1,23 @@
 class AttachmentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_attachment
+  before_action :verify_authorship
+
+  respond_to :js
 
   def destroy
+    respond_with(@attachment.destroy)
+  end
+
+  private
+
+  def set_attachment
     @attachment = Attachment.find(params[:id])
-    @attachment.destroy if current_user.author_of?(@attachment.attachable)
+  end
+
+  def verify_authorship
+    unless current_user.author_of?(@attachment.attachable)
+      render :destroy
+    end
   end
 end
