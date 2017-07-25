00c74af7bbf05d4e8b63a0012919191c236a4697
Mirbek Askerov <mirbek.askerov@gmail.com>
2017-07-25 11:09:53 +0600

    Refactoring. Omniauth_callbacks_controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,19 +1,17 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def facebook
-    define_oauth_from(:facebook)
-  end
+  before_action :define_oauth_from
 
-  def twitter
-    define_oauth_from(:twitter)
-  end
+  def facebook; end
+
+  def twitter; end
 
   private
 
-  def define_oauth_from(provider)
+  def define_oauth_from
     @user = User.find_for_oauth(request.env['omniauth.auth'])
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: provider.to_s) if is_navigational_format?
+      set_flash_message(:notice, :success, kind: request.env['omniauth.auth'] ['provider']) if is_navigational_format?
     end
   end
 end
